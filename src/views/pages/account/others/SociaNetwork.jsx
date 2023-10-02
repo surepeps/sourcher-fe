3a2411ad941235d6298ffe729988cdff31737f6c
surecoder <gatukurh1@gmail.com>
2023-10-02 10:34:54 +0100

    erfdss

--- src/views/pages/account/others/SociaNetwork.jsx
+++ src/views/pages/account/others/SociaNetwork.jsx
@@ -1,59 +1,109 @@
-import React from 'react'
+import React, { useCallback } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import { useAuth } from '../../../../context/AuthContext';
+import { useRequestLoading } from '../../../../context/LoadingContext';
 
-function SociaNetwork({ProfileData}) {
-    const {facebook,google,twitter,linkedin,youtube,instagram} = ProfileData;
+// Reusable SocialLinkInput component
+function SocialLinkInput({ label, name, value, onChange, error }) {
+  const memoizedOnChange = useCallback(
+    (event) => {
+      onChange(event.target.value);
+    },
+    [onChange]
+  );
+
   return (
-    <form className='flex justify-between gap-10 lg:gap-20 flex-col lg:flex-row'>
-        <div className="leftF w-full flex flex-col gap-5">
-
-            <div className="singleF flex flex-col gap-2 w-full">
-                <label className='text-sm font-semibold' htmlFor="">Linkedln URL</label>
-                <div className="formGroup flex w-full">
-                    <label htmlFor="" className='h-full py-5 text-sm px-8 rounded-l-lg bg-[#F9FAFB]'>https://</label>
-                    <input type="text" className='h-auto px-3 lg:w-6/12 w-full rounded-r-lg border border-[#D9D9D9] text-sm' />
-                </div>
-                
-            </div>
-
-            <div className="singleF flex flex-col gap-2 w-full">
-                <label className='text-sm font-semibold' htmlFor="">Twitter</label>
-                <div className="formGroup flex ">
-                    <label htmlFor="" className='h-full py-5 text-sm px-8 rounded-l-lg bg-[#F9FAFB]'>https://</label>
-                    <input type="text" className='h-auto px-3 lg:w-6/12 w-full rounded-r-lg border border-[#D9D9D9] text-sm' />
-                </div>
-                
-            </div>
-
-            <div className="singleF flex flex-col gap-2">
-                <label className='text-sm font-semibold' htmlFor="">Instagram</label>
-                <div className="formGroup flex ">
-                    <label htmlFor="" className='h-full py-5 text-sm px-8 rounded-l-lg bg-[#F9FAFB]'>https://</label>
-                    <input type="text" className='h-auto px-3 lg:w-6/12 w-full rounded-r-lg border border-[#D9D9D9] text-sm' />
-                </div>
-                
-            </div>
-
-            <div className="singleF flex flex-col gap-2">
-                <label className='text-sm font-semibold' htmlFor="">Personal/Any Website URL</label>
-                <div className="formGroup flex ">
-                    <label htmlFor="" className='h-full py-5 text-sm px-8 rounded-l-lg bg-[#F9FAFB]'>https://</label>
-                    <input type="text" className='h-auto px-3 lg:w-6/12 w-full rounded-r-lg border border-[#D9D9D9] text-sm' />
-                </div>
-                
-            </div>
-
-        </div>
-
-        <div className="rightF">
-            <button className='border whitespace-nowrap border-awimGreen bg-awimGreen text-white text-sm hover:text-awimGreen hover:bg-transparent transition duration-300 ease-in-out rounded-lg px-8 py-3'>
-                Update Links
-            </button>
-        </div>
-
-    </form>
-  )
+    <div className="singleF flex flex-col gap-2 w-full">
+      <label className='text-sm font-semibold' htmlFor={name}>{label}</label>
+      <div className="formGroup flex w-full">
+        <label htmlFor={name} className='h-full py-5 text-sm px-8 rounded-l-lg bg-[#F9FAFB]'>https://</label>
+        <input
+          type="text"
+          id={name}
+          name={name}
+          className='h-auto px-3 lg:w-6/12 w-full rounded-r-lg border border-[#D9D9D9] text-sm'
+          onChange={memoizedOnChange}
+          value={value}
+        />
+      </div>
+      {error && <div className="text-red-600 text-xs">{error}</div>}
+    </div>
+  );
 }
 
-export default SociaNetwork
+function SociaNetwork({ ProfileData }) {
+    const {updateUserData} = useAuth()
+    const { setRequestLoading } = useRequestLoading();
+
+  const socialLinks = [
+    { name: 'linkedin', label: 'LinkedIn URL' },
+    { name: 'twitter', label: 'Twitter URL' },
+    { name: 'instagram', label: 'Instagram URL' },
+    { name: 'webiste', label: 'Personal/Any Website URL' },
+  ];
+
+  // Custom validation function to ignore http:// or https://
+  const urlValidation = Yup.string().test('is-url', 'Invalid URL', (value) => {
+    if (!value) {
+      return true; // Empty value is valid
+    }
+    // Check if the value starts with http:// or https:// and return true if it does
+    return !value.startsWith('http://') && !value.startsWith('https://');
+  });
+
+  const validationSchema = Yup.object().shape(
+    socialLinks.reduce((schema, { name }) => ({
+      ...schema,
+      [name]: urlValidation,
+    }), {})
+  );
+
+  const formik = useFormik({
+    initialValues: socialLinks.reduce((values, { name }) => ({
+      ...values,
+      [name]: ProfileData[name] || '',
+    }), {}),
+    validationSchema,
+    onSubmit: (values) => {
+      try {
+        setRequestLoading(true);
+        updateUserData({updatedUserData:values});
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setRequestLoading(false);
+      }
+    },
+  });
+
+  return (
+    <div className="f">
+        <form onSubmit={formik.handleSubmit} className='flex justify-between gap-10 lg:gap-20 flex-col lg:flex-row'>
+            <div className="w-full flex gap-8 flex-col">
+                {socialLinks.map(({ name, label }) => (
+                <SocialLinkInput
+                    key={name}
+                    label={label}
+                    name={name}
+                    value={formik.values[name]}
+                    onChange={(value) => formik.setFieldValue(name, value)} // Update using setFieldValue
+                    error={formik.touched[name] && formik.errors[name]}
+                />
+                ))}
+            </div>
+
+            <div className="rightF">
+                <button className='border whitespace-nowrap border-awimGreen bg-awimGreen text-white text-sm hover:text-awimGreen hover:bg-transparent transition duration-300 ease-in-out rounded-lg px-8 py-3'>
+                Update Links
+                </button>
+            </div>
+        </form>
+
+        <div className="line my-14 w-full bg-[#0F172A13] h-0.5 rounded-xl"></div>
+    </div>
+    
+  );
+}
+
+export default SociaNetwork;
--- src/views/pages/account/user_level/Links.jsx
+++ src/views/pages/account/user_level/Links.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SociaNetwork from '../others/SociaNetwork'
+import Publications from '../others/Publications';
 
 function Links({allData}) {
   const {ProfileData, myData, config, isProfileLevel1, isProfileLevel2, isMyAccount} = allData;
@@ -7,6 +8,8 @@ function Links({allData}) {
   return (
     <div className='font-notoSans mt-5'>
       { isMyAccount ? <SociaNetwork ProfileData={ProfileData} /> : '' }
+
+      <Publications />
       
     </div>
   )
