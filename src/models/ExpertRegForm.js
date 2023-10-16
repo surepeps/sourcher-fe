a39e43a99a5e0ecc5da9080fa0e3faf946fb1a13
surecoder <gatukurh1@gmail.com>
2023-10-16 22:35:29 +0100

    rgnbddbghnng

--- src/models/ExpertRegForm.js
+++ src/models/ExpertRegForm.js
@@ -1,6 +1,56 @@
 export const ExpertRegForm = [
-    {},
-    {},
-    {},
-    {},
+    {
+        name: 'first_name',
+        label: 'First Name',
+        type: 'text',
+        placeholder: 'Enter First Name',
+      },
+      {
+        name: 'last_name',
+        label: 'Last Name',
+        type: 'text',
+        placeholder: 'Enter Last Name',
+      },
+      {
+        name: 'email_address',
+        label: 'Email Address',
+        type: 'email',
+        placeholder: 'Enter Email Address',
+      },
+      {
+        name: 'phone_number',
+        label: 'Phone Number',
+        type: 'text',
+        placeholder: 'Enter Phone Number',
+      },
+      {
+        name: 'country_code',
+        label: 'Country of Origin',
+        type: 'select',
+        placeholder: 'Select Country',
+      },
+      {
+        name: 'country_state',
+        label: 'State',
+        type: 'select',
+        placeholder: 'Select State',
+      },
+      {
+        name: 'city_town',
+        label: 'City / Town',
+        type: 'select',
+        placeholder: 'Select City',
+      },
+      {
+        name: 'password',
+        label: 'Password',
+        type: 'password',
+        placeholder: 'Enter Password',
+      },
+      {
+        name: 'c_password',
+        label: 'Confirm Password',
+        type: 'password',
+        placeholder: 'Enter Confirm Password',
+      },
 ];
--- src/views/pages/auth/ExpertRegister.jsx
+++ src/views/pages/auth/ExpertRegister.jsx
@@ -1,8 +1,40 @@
-import React, {useEffect, useState} from 'react'
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import { Country, State, City }  from 'country-state-city';
+import { Country, State, City } from 'country-state-city';
+import { ExpertRegForm } from '../../../models/ExpertRegForm';
+
+
+const initialValues = {};
+ExpertRegForm.forEach((field) => {
+  initialValues[field.name] = '';
+});
+
+const generateValidationSchema = (fields) => {
+  const schema = {};
+
+  fields.forEach((field) => {
+    schema[field.name] = Yup.string().required(`${field.label} is required`);
+
+    if (field.type === 'email') {
+      schema[field.name] = schema[field.name].email('Invalid email address');
+    } else if (field.name === 'password') {
+      schema[field.name] = schema[field.name]
+        .matches(
+          /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+          'Password must be at least 8 characters and contain a mixture of alphabets, numbers, and special characters'
+        )
+        .required('Password is required');
+    } else if (field.name === 'phone_number') {
+      schema[field.name] = schema[field.name]
+        .matches(/^\+?\d{10,14}$/, 'Invalid phone number format')
+        .required('Phone Number is required');
+    }
+  });
+
+  return Yup.object().shape(schema);
+};
 
 function ExpertRegister() {
   const [selectedCountry, setSelectedCountry] = useState('');
@@ -12,7 +44,6 @@ function ExpertRegister() {
   const [states, setStates] = useState([]);
   const [cities, setCities] = useState([]);
 
-
   useEffect(() => {
     const countryData = Country.getAllCountries();
     setCountries(countryData);
@@ -20,12 +51,12 @@ function ExpertRegister() {
 
   const handleCountryChange = (event) => {
     const country = event.target.value;
-    console.log("Selected Country", country);
     setSelectedCountry(country);
     setSelectedState('');
     setSelectedCity('');
 
     const stateData = State.getStatesOfCountry(country);
+    console.log(stateData);
     setStates(stateData);
     setCities([]);
   };
@@ -39,28 +70,11 @@ function ExpertRegister() {
     setCities(cityData);
   };
 
+  const validationSchema = generateValidationSchema(ExpertRegForm);
+
   const formik = useFormik({
-    initialValues: {
-      first_name: '',
-      last_name: '',
-      email_address: '',
-      phone_number: '',
-      password: '',
-      c_password: '',
-    },
-    validationSchema: Yup.object({
-      first_name: Yup.string().required('First Name is required'),
-      last_name: Yup.string().required('Last Name is required'),
-      email_address: Yup.string().email('Invalid email address').required('Email Address is required'),
-      phone_number: Yup.string().required('Phone Number is required'),
-      country_code: Yup.string().required('Country of Origin is required'),
-      country_state: Yup.string().required('State is required'),
-      city_town: Yup.string().required('City/Town is required'),
-      password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
-      c_password: Yup.string()
-        .oneOf([Yup.ref('password'), null], 'Passwords must match')
-        .required('Confirm Password is required'),
-    }),
+    initialValues,
+    validationSchema: validationSchema,
     onSubmit: (values) => {
       // Handle form submission here
       console.log('Form values:', values);
@@ -68,186 +82,82 @@ function ExpertRegister() {
   });
 
   return (
-    <div className='pt-6'>
+    <div className="pt-6">
       <div className="text-3xl xl:text-5xl font-semibold text-center lg:text-left">Expert Sign Up</div>
-      <p className='pt-4'>We aim to create a networking/collaboration ground for African Women Experts like you to contribute your voices to our stories</p>
+      <p className="pt-4">We aim to create a networking/collaboration ground for African Women Experts like you to contribute your voices to our stories</p>
 
-      <form onSubmit={formik.handleSubmit} className='pt-14 lg:pt-20 px-2'>
-        {/* Render form fields using map or a component */}
-        <div className="mb-5">
-          <label className={`flex justify-between text-gray-700 text-xs lg:text-sm font-bold mb-2`} htmlFor="first_name">
-              First Name
-              <span className="text-red-500 text-xs">First Name is required</span>
-          </label>
+      <form onSubmit={formik.handleSubmit} className="pt-14 lg:pt-20 px-2">
+        {ExpertRegForm.map((field) => (
+          <div key={field.name} className="mb-5">
+            <label className="flex justify-between text-gray-700 text-xs lg:text-sm font-bold mb-2" htmlFor={field.name}>
+              {field.label}
+              {formik.errors[field.name] && formik.touched[field.name] && (
+                <span className="text-red-500 text-xs">{formik.errors[field.name]}</span>
+              )}
+            </label>
 
-          <input
-                value=""
-                id="first_name"
-                name="first_name"
-                type='text'
-                className={`w-full py-5 rounded-lg px-4 text-xs lg:text-sm text-gray-700 focus:outline-none focus:shadow-outline`}
-                placeholder="Enter First Name"
+            {field.type === 'select' ? (
+              <select
+                name={field.name}
+                id={field.name}
+                value={formik.values[field.name]}
+                onChange={field.name === 'country_code' ? handleCountryChange : field.name === 'country_state' ? handleStateChange : formik.handleChange}
+                className="w-full py-5 rounded-lg px-4 text-xs lg:text-sm text-gray-700 focus:outline-none focus:shadow-outline"
+              >
+                <option value="">{field.placeholder}</option>
+                {field.name === 'country_code'
+                  ? countries.map((country) => (
+                      <option key={country.isoCode} value={country.isoCode}>
+                        {country.name}
+                      </option>
+                    ))
+                  : field.name === 'country_state' && selectedCountry
+                  ? states.map((state) => (
+                      <option key={state.name} value={state.isoCode}>
+                        {state.name}
+                      </option>
+                    ))
+                  : field.name === 'city_town' && selectedState && (
+                      cities.map((city) => (
+                        <option key={city.name} value={city.name}>
+                          {city.name}
+                        </option>
+                      ))
+                    )}
+              </select>
+            ) : (
+              <input
+                id={field.name}
+                name={field.name}
+                type={field.type}
+                value={formik.values[field.name]}
+                onChange={formik.handleChange}
+                className="w-full py-5 rounded-lg px-4 text-xs lg:text-sm text-gray-700 focus:outline-none focus:shadow-outline"
+                placeholder={field.placeholder}
               />
-
-        </div>
-
-        <div className="mb-5">
-          <label className={`flex justify-between text-gray-700 text-xs lg:text-sm font-bold mb-2`} htmlFor="last_name">
-              Last Name
-              <span className="text-red-500 text-xs">Last Name is required</span>
-          </label>
-
-          <input
-                value=""
-                id="last_name"
-                name="last_name"
-                type='text'
-                className={`w-full py-5 rounded-lg px-4 text-xs lg:text-sm text-gray-700 focus:outline-none focus:shadow-outline`}
-                placeholder="Enter Last Name"
-              />
-
-        </div>
-
-        <div className="mb-5">
-          <label className={`flex justify-between text-gray-700 text-xs lg:text-sm font-bold mb-2`} htmlFor="email_address">
-              Email Address
-              <span className="text-red-500 text-xs">Email Address is required</span>
-          </label>
-
-          <input
-                value=""
-                id="email_address"
-                name="email_address"
-                type='email'
-                className={`w-full py-5 rounded-lg px-4 text-xs lg:text-sm text-gray-700 focus:outline-none focus:shadow-outline`}
-                placeholder="Enter Email Address"
-              />
-
-        </div>
-
-        <div className="mb-5">
-          <label className={`flex justify-between text-gray-700 text-xs lg:text-sm font-bold mb-2`} htmlFor="phone_number">
-              Phone Number 
-              <span className="text-red-500 text-xs">Phone Number is required</span>
-          </label>
-
-          <input
-                value=""
-                id="phone_number"
-                name="phone_number"
-                type='number'
-                className={`w-full py-5 rounded-lg px-4 text-xs lg:text-sm text-gray-700 focus:outline-none focus:shadow-outline`}
-                placeholder="Enter Phone Number"
-              />
-
-        </div>
-
-        <div className="mb-5">
-          <label className={`flex justify-between text-gray-700 text-xs lg:text-sm font-bold mb-2`} htmlFor="country_code">
-              Country of Origin
-              <span className="text-red-500 text-xs">Country of Origin is required</span>
-          </label>
-          <select name="country_code"  value={selectedCountry} onChange={handleCountryChange} id="country_code" className={`w-full py-5 rounded-lg px-4 text-xs lg:text-sm text-gray-700 focus:outline-none focus:shadow-outline`}>
-            <option value="">Select Country</option>
-            {countries.map((country) => (
-              <option key={country.isoCode} value={country.isoCode}>
-                {country.name}
-              </option>
-            ))}
-          </select>
-
-        </div>
-
-        <div className="mb-5">
-          <label className={`flex justify-between text-gray-700 text-xs lg:text-sm font-bold mb-2`} htmlFor="country_state">
-              State
-              <span className="text-red-500 text-xs">State is required</span>
-          </label>
-          <select name="country_state" id="country_state" value={selectedState} onChange={handleStateChange} className={`w-full py-5 rounded-lg px-4 text-xs lg:text-sm text-gray-700 focus:outline-none focus:shadow-outline`}>
-            {selectedCountry && (
-              <>
-                <option value="">Select State</option>
-                {states.map((state) => (
-                  <option key={state.name} value={state.isoCode}>
-                    {state.name}
-                  </option>
-                ))}
-              </>
             )}
-          </select>
+          </div>
+        ))}
 
-        </div>
-
-        <div className="mb-5">
-          <label className={`flex justify-between text-gray-700 text-xs lg:text-sm font-bold mb-2`} htmlFor="city_town">
-              City / Town
-              <span className="text-red-500 text-xs">City/Town is required</span>
-          </label>
-          <select name="city_town" id="city_town"  value={selectedCity} onChange={(event) => setSelectedCity(event.target.value)} className={`w-full py-5 rounded-lg px-4 text-xs lg:text-sm text-gray-700 focus:outline-none focus:shadow-outline`}>
-            {selectedState && (
-              <>
-                <option value="">Select City</option>
-                {cities.map((city) => (
-                  <option key={city.name} value={city.name}>
-                    {city.name}
-                  </option>
-                ))}
-              </>
-            )}
-          </select>
-
-        </div>
-
-        <div className="mb-5">
-          <label className={`flex justify-between text-gray-700 text-xs lg:text-sm font-bold mb-2`} htmlFor="password">
-              Password 
-              <span className="text-red-500 text-xs">Password is required</span>
-          </label>
-
-          <input
-                value=""
-                id="password"
-                name="password"
-                type='password'
-                className={`w-full py-5 rounded-lg px-4 text-xs lg:text-sm text-gray-700 focus:outline-none focus:shadow-outline`}
-                placeholder="Enter Password"
-              />
-
-        </div>
-
-        <div className="mb-5">
-          <label className={`flex justify-between text-gray-700 text-xs lg:text-sm font-bold mb-2`} htmlFor="c_password">
-              Confirm Password 
-              <span className="text-red-500 text-xs">Confirm Password is required</span>
-          </label>
-
-          <input
-                value=""
-                id="c_password"
-                name="c_password"
-                type='password'
-                className={`w-full py-5 rounded-lg px-4 text-xs lg:text-sm text-gray-700 focus:outline-none focus:shadow-outline`}
-                placeholder="Enter Confirm Password"
-              />
-
-        </div>
-
-      
         <div className="mt-10">
           <button
             type="submit"
-            className='w-full py-5 rounded-lg px-4 text-sm text-white bg-awimGreen hover:bg-awimFadeGreen focus:outline-none focus:shadow-outline transition duration-300 ease-in-out'
+            className="w-full py-5 rounded-lg px-4 text-sm text-white bg-awimGreen hover:bg-awimFadeGreen focus:outline-none focus:shadow-outline transition duration-300 ease-in-out"
           >
             Sign Up
           </button>
         </div>
 
         <div className="flex justify-center pt-6 text-md">
-          <p> Already have an Expert account? <NavLink to='/login' className='text-awimGreen'>Login</NavLink> </p>
+          <p>
+            Already have an Expert account? <NavLink to="/login" className="text-awimGreen">
+              Login
+            </NavLink>
+          </p>
         </div>
       </form>
     </div>
-  )
+  );
 }
 
-export default ExpertRegister
+export default ExpertRegister;
